0408278fff77858c2fe84532b0e4f2a81c0b4834
Yunus Emre Gül <yunemregul@gmail.com>
2020-12-20 13:55:36 +0300

    Mouse movement basics

--- desktop/src/main.ts
+++ desktop/src/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from "electron";
 import * as WebSocket from 'ws';
 import * as http from 'http';
+import * as robot from 'robotjs';
 
 const PORT = 41414;
 
@@ -11,9 +12,16 @@ const server = http.createServer(function (req, res) {
 
 const wss = new WebSocket.Server({ server });
 
+// TODO: it doesn't work as expected
+
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+  ws.on('message', function incoming(message : string) {
+    let splitted = message.split(':');
+    let x = Number(message[0]);
+    let y = Number(message[1]);
+    console.log(x,y);
     
+    robot.moveMouse(robot.getMousePos().x + 1, robot.getMousePos().y + 1);
   });
 });
 
--- mobilemouse/App.tsx
+++ mobilemouse/App.tsx
@@ -1,11 +1,26 @@
-import React, {useEffect} from 'react';
-import {SafeAreaView, Text, View, AppRegistry} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  AppRegistry,
+  TouchableOpacity,
+} from 'react-native';
 import {NetworkInfo} from 'react-native-network-info';
 import {name as appName} from './app.json';
 
 const PORT = 41414;
 
-const App: () => React.ReactElement = () => {
+// TODO: refresh connectable desktop IPs every x secs
+// TODO: separate this file into smaller components, improve the UI
+
+let ws: WebSocket;
+let dragLastPointX: number, dragLastPointY: number; // TODO: join these two in a single object
+
+function App() {
+  const [desktopIPs, setDesktopIPs] = useState<string[]>([]);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
   useEffect(() => {
     NetworkInfo.getIPV4Address().then((ipAddress) => {
       if (ipAddress) {
@@ -16,7 +31,7 @@ const App: () => React.ReactElement = () => {
 
           fetch('http://' + ipToScan + ':' + PORT)
             .then(() => {
-              console.log(ipToScan);
+              setDesktopIPs((desktopIPs) => [...desktopIPs, ipToScan]);
             })
             .catch(() => {});
         }
@@ -26,17 +41,71 @@ const App: () => React.ReactElement = () => {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View
-        style={{
-          backgroundColor: '#444',
-          flex: 1,
-        }}
-        onMoveShouldSetResponder={(evt) => true}
-        onResponderMove={(evt) => {
-          //console.log(evt);
-        }}></View>
+      {isConnected ? (
+        <>
+          <View
+            style={{
+              backgroundColor: '#222',
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}
+            onMoveShouldSetResponder={() => true}
+            onResponderMove={(e) => {
+              // TODO: doesn't work as expected
+              ws.send(
+                e.nativeEvent.pageX -
+                  dragLastPointX +
+                  ':' +
+                  (e.nativeEvent.pageY - dragLastPointY),
+              );
+              
+              dragLastPointX = e.nativeEvent.pageX;
+              dragLastPointY = e.nativeEvent.pageY;
+            }}>
+            <Text style={{color: '#fff', top: 60, textAlign: 'center'}}>
+              Drag around to control your mouse!
+            </Text>
+          </View>
+        </>
+      ) : (
+        <>
+          <View
+            style={{
+              backgroundColor: '#444',
+              flex: 1,
+              flexDirection: 'row',
+            }}>
+            <View style={{padding: 12}}>
+              <Text style={{color: '#fff', fontSize: 20}}>
+                Connectable devices:
+              </Text>
+              <View style={{display: 'flex', flexDirection: 'row'}}>
+                {desktopIPs.map((ip) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      ws = new WebSocket('ws://' + ip + ':' + PORT);
+                      setIsConnected(true);
+                    }}
+                    key={ip}
+                    style={{
+                      backgroundColor: '#555',
+                      padding: 12,
+                      width: '100%',
+                      marginTop: 8,
+                    }}>
+                    <View style={{}}>
+                      <Text style={{color: '#00ff00', fontSize: 16}}>{ip}</Text>
+                    </View>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            </View>
+          </View>
+        </>
+      )}
     </SafeAreaView>
   );
-};
+}
 
 AppRegistry.registerComponent(appName, () => App);
